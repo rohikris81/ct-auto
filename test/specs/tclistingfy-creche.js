4228366362247c2eb1fd162ea418e89d6b3ce2a6
Rohit Krishna <rohitkrishna@ip-192-168-1-106.us-west-2.compute.internal>
2022-05-27 19:48:57 +0530

    add cases for creche site

--- test/specs/tclistingfy-creche.js
+++ test/specs/tclistingfy-creche.js
@@ -1,9 +1,9 @@
 var Listingfy = require('../pageobjects/listingfy.page');
 var data = require('../data/testdata.json');
 
-describe('Verification of Landing Page - Listingfy Creche', ()=>{
+describe('Verification of Landing Page - Listingfy Creche', () => {
 
-    it('To verify navigation presence', async ()=>{
+    it('To verify navigation presence', async () => {
         await Listingfy.openListingfyCreche();
         await expect(Listingfy.nav_about()).toBeDisplayed();
         await expect(Listingfy.nav_about()).toBeClickable();
@@ -17,16 +17,16 @@ describe('Verification of Landing Page - Listingfy Creche', ()=>{
         await expect(Listingfy.nav_learning()).toBeClickable();
         await expect(Listingfy.nav_programme()).toBeDisplayed();
         await expect(Listingfy.nav_programme()).toBeClickable();
-    });   
+    });
 
-    it('To verify Query Submission', async ()=>{
+    it('To verify Query Submission', async () => {
         await Listingfy.openListingfyCreche();
-      await Listingfy.submitQuery("rk","ss@ss.com","9826567763","hi message","sub");
-      await browser.pause(2000);
-      //await expect($('#contact-form-box').getText()).toEqual("Query submitted successfully");
-      await expect(Listingfy.buttonPresent()).not.toBeDisplayed();
-      
-    });   
+        await Listingfy.submitQuery(data.listingycreche.name_valid, data.listingycreche.email_valid, data.listingycreche.mobileno_valid, data.listingycreche.subject_valid, data.listingycreche.subject_valid);
+        await browser.pause(2000);
+        //await expect($('#contact-form-box').getText()).toEqual("Query submitted successfully");
+        await expect(Listingfy.buttonPresent()).not.toBeDisplayed();
+
+    });
 
 });
 
