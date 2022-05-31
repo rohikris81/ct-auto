d6e21aa937dc1b70807998fc4b293e194323003e
Rohit Krishna <rohitkrishna@ip-10-65-21-96.us-west-2.compute.internal>
2022-05-31 15:10:11 +0530

    new code added

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,6 +20,13 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
+    reporters: [
+        'dot',
+        ['junit', {
+            outputDir: './'
+        }]
+    ],
+
     specs: [
         './test/specs/**/*.js'
     ],
@@ -57,6 +64,9 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
+         'goog:chromeOptions': { 
+            args: ["--headless", "user-agent=...","--disable-gpu","--window-size=1440,735"]
+       },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
